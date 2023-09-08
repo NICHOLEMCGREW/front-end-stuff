84ae31b29b590ec0ae4984c7184c6f6b29fa06ef
Nichole <nicholemcgrew@gmail.com>
2023-09-08 10:09:29 -0500

    reviewed code

--- class-31-32/p.js
+++ class-31-32/p.js
@@ -35,3 +35,59 @@ class Dog extends DomesticAnimal {
 }
 
 let simba = new Dog('Simba', 'Shepard', 'Large')
+
+
+
+class Contractor {
+    constructor(name, role) {
+        this._name = name
+        this._role = role
+    }
+    get name() {
+        return this._name
+    }
+    get role() {
+        return this._role
+    }
+    sayHello() {
+        console.log(`Hello, my name is ${this._name} and I'm on the ${this._role} team!`)
+    }
+    sayBye() {
+        console.log(`It was a pleasure working with you!`)
+    }
+}
+
+class Front extends Contractor {
+    constructor(name, role, tech) {
+        super(name, role)
+        this._tech = tech
+    }
+    get tech() {
+        return this._tech
+    }
+    sayHello() {
+        console.log(`Hello, I am on the ${this.role} team at #100Devs and I use ${this._tech}`)
+    }
+}
+
+class Back extends Contractor {
+    constructor(name, role, tech) {
+        super(name, role)
+        this._tech = tech
+    }
+    get tech() {
+        return this._tech
+    }
+    sayHello() {
+        console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+    }
+}
+
+let machi = new Contractor('The Machine', 'Front-end')
+let simbas = new Back('Simba', 'Back-end Developer', 'React')
+
+let agencyList = [machi, simba]
+
+for (person of agencyList) {
+    person.sayHello()
+}
