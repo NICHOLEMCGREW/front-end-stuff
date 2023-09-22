22f11afb47e4ef10a3c9eb4679640d3cf3c811c9
Nichole <nicholemcgrew@gmail.com>
2023-09-22 09:25:09 -0500

    finished super review

--- SuperReview/review-in-paradise-after-paradise/js/main.js
+++ SuperReview/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,12 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+function comapareArr(arr) {
+    if(arr[0] < arr[arr.length-1]) {
+        console.log('hi')
+    } else if (arr[0] > arr[arr.length-1]) {
+        console.log('bye')
+    } else {
+        console.log("We close in an hour")
+    }
+}
+comapareArr([1,2,3,4,1])
--- SuperReview/review-winter-games/js/main.js
+++ SuperReview/review-winter-games/js/main.js
@@ -1,1 +1,21 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+function arrayOfNums(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            arr.splice(i, 1)
+        }
+    }
+    return arr;
+}
+console.log(arrayOfNums([1,2,3,4,1]))
+
+function onlyEvens(arr) {
+    let onlyEvens = []
+    arr.forEach(e => {
+        if (e % 2 === 0) {
+            onlyEvens.push(e)
+        }
+    })
+    return onlyEvens
+}
