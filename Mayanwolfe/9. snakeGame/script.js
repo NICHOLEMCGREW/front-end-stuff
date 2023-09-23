ba725dff739764514665847f64bcf8d6858b2dd9
Nichole <nicholemcgrew@gmail.com>
2023-09-23 09:01:15 -0500

    synced code

--- Mayanwolfe/9. snakeGame/script.js
+++ Mayanwolfe/9. snakeGame/script.js
@@ -1,14 +1,17 @@
-const LINE_PIXEL_COUNT = 40;
-const TOTAL_PIXEL_COUNT = LINE_PIXEL_COUNT**2;
+//Declare global variables to track game board size
+const LINE_PIXEL_COUNT = 40
+const TOTAL_PIXEL_COUNT = LINE_PIXEL_COUNT**2
 
-let totalFoodEaten = 0;
-let totalDistanceTraveled = 0;
+//Track scores to display to user
+let totalFoodEaten = 0
+let totalDistanceTraveled = 0
 
-const gameContainer = document.getElementById('gameContainer');
+//Shorten reference to game board
+const gameContainer = document.getElementById('gameContainer')
 
+//Generate the game board
 const createGameBoardPixels = () => {
-    for (let i = 1; i <= TOTAL_PIXEL_COUNT; i++) {
-        gameContainer.innerHTML = `${gameContainer.innerHTML} 
-        <div class="gameBoardPixel" id = "pixel${i}"></div>`; 
-    }
+  for (let i = 1; i<= TOTAL_PIXEL_COUNT; i++) {
+    gameContainer.innerHTML = `${gameContainer.innerHTML} <div class="gameBoardPixel" id = "pixel${i}"></div>`;
+  }
 }
