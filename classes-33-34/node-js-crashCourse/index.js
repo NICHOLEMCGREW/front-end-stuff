d6c7a48b61304319541c5792f72f97a8df5c4690
Nichole <nicholemcgrew@gmail.com>
2023-09-27 05:16:12 -0600

    halfway through crash course

--- classes-33-34/node-js-crashCourse/index.js
+++ classes-33-34/node-js-crashCourse/index.js
@@ -1,1 +1,10 @@
-ECHO is on.
+// Import isn't available to Node yet
+// import Person from './person'; 
+
+//common js
+const Person = require('./person');
+
+
+const person1 = new Person('John Doe', 30);
+
+person1.greeting();
